85d92b42d5635eb117aa4bb02d7b6a6752c984cc
Cali Knowlden <calbearnc@gmail.com>
2024-04-21 20:31:57 -0500

    added input field for json page

--- src/components/JSON/JSON.jsx
+++ src/components/JSON/JSON.jsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import './JSON.css';
 
 function JSON() {
-  return <div>Input your JSON file here</div>;
+  const dispatch = useDispatch();
+  const [jsonInput, setJsonInput] = useState('');
+
+  const submitInput = (event) => {
+    event.preventDefault();
+    //TO DO: dispatch to saga
+  };
+
+  return (
+    <div>
+      <p>Input your JSON file here</p>
+      <br />
+      <center>
+        <form onSubmit={submitInput}>
+          <textarea
+            className="json-form"
+            rows={25}
+            cols={80}
+            name="jsonInput"
+            value={jsonInput}
+            required
+            placeholder="Enter JSON"
+            onChange={(event) => setJsonInput(event.target.value)}
+          />
+          <div>
+            <input className="btn" type="submit" />
+          </div>
+        </form>
+      </center>
+    </div>
+  );
 }
 
 export default JSON;
