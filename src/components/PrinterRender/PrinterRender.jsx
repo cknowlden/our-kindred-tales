7036aef89e029438436ca498b149daa881a9a23d
Cali Knowlden <calbearnc@gmail.com>
2024-04-24 10:09:03 -0500

    added temporary go back button for printer render

--- src/components/PrinterRender/PrinterRender.jsx
+++ src/components/PrinterRender/PrinterRender.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function PrinterRender() {
   return (
     <div>
       <h1>View printer friendly PDF render here.</h1>
       {/* TO DO: change button route to go back to associated project details page base on id */}
-      <button className="btn">Go Back</button>
+      <Link to="/overview">
+        <button className="btn">Go Back</button>{' '}
+      </Link>
     </div>
   );
 }
