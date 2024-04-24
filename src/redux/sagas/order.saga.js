6b014c6bbc7d19ef6a22ab0515243de381f405f2
Daniel Holt <holt.daniel.b@gmail.com>
2024-04-24 18:34:27 -0400

    lulu token retrieval added to order saga

--- src/redux/sagas/order.saga.js
+++ src/redux/sagas/order.saga.js
@@ -5,8 +5,25 @@ import axios from 'axios';
 function* submitOrder(action) {
   try {
     // Retrieve security token
-
+    const postData = new URLSearchParams();
+    postData.append('grant_type', 'client_credentials');
+    
+    const config = {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ZjJjNDdmMTctOWMxZi00ZWZlLWIzYzEtMDI4YTNlZTRjM2M3OjMzOTViZGU4LTBkMjQtNGQ0Ny1hYTRjLWM4NGM3NjI0OGRiYw=='
+      }
+    };
+    
+    axios.post('https://api.lulu.com/auth/realms/glasstree/protocol/openid-connect/token', postData, config)
+      .then(response => {
+        console.log('Response:', response.data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
     // Send order to lulu
+    
 
     // Retrieve order accepted status
   } catch (error) {
