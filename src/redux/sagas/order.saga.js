2d7ae1adc8a6108bf3de00ade8db2385fe1c6e34
Daniel Holt <holt.daniel.b@gmail.com>
2024-04-27 14:40:14 -0400

    authentication retrieved and order request being sent

--- src/redux/sagas/order.saga.js
+++ src/redux/sagas/order.saga.js
@@ -1,39 +1,53 @@
-import { put, takeLatest } from 'redux-saga/effects';
-import axios from 'axios';
+// DON'T FORGET TO REPLACE SANDBOX URL WITH REAL LULU FOR CLIENT
+
+import { put, takeLatest } from "redux-saga/effects";
+import axios from "axios";
 // worker Saga: will be fired on "ORDER" actions
 function* submitOrder(action) {
-  let access_token = {
-
-  }
+  const url = "https://api.sandbox.lulu.com";
+  let accessToken = '';
   try {
     // Retrieve security token
-    const url = 'https://api.sandbox.lulu.com/auth/realms/glasstree/protocol/openid-connect/token';
-    const data = 'grant_type=client_credentials';
+    const grantData = "grant_type=client_credentials";
     const headers = {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Authorization': 'Basic MjlmYTQzNDYtNTBiMC00NDRlLTgwNjUtYmNhOGMyOGMwMTMxOmRhOFB3NzBseVdJT2ZlUVg3TVpwMlVMNkw3cUNaOTlN'
+      "Content-Type": "application/x-www-form-urlencoded",
+      'Authorization': "Basic MjlmYTQzNDYtNTBiMC00NDRlLTgwNjUtYmNhOGMyOGMwMTMxOmRhOFB3NzBseVdJT2ZlUVg3TVpwMlVMNkw3cUNaOTlN",
     };
-    axios.post(url, data, { headers })
+    yield axios
+      .post(
+        `${url}/auth/realms/glasstree/protocol/openid-connect/token`,
+        grantData,
+        { headers }
+      )
+      .then((response) => {
+        accessToken = response.data;
+        console.log("Response:", accessToken);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+    // Send order to lulu
+    const data = ''
+    axios.post(`${url}/print-jobs/`, data , headers = {
+      'Authorization': `Bearer ${accessToken}`,
+      'Content-Type': 'application/json'
+    })
       .then(response => {
         console.log('Response:', response.data);
-        access_token = response.data.access_token;
       })
       .catch(error => {
         console.error('Error:', error);
       });
     
-    // Send order to lulu
-    
-    
     // Retrieve order accepted status
   } catch (error) {
-    console.log('Error with order submit:', error);
-    yield put({ type: 'ORDER_FAILED' });
+    console.log("Error with order submit:", error);
+    yield put({ type: "ORDER_FAILED" });
   }
 }
 
 function* orderSaga() {
-  yield takeLatest('SUBMIT_ORDER', submitOrder);
+  yield takeLatest("SUBMIT_ORDER", submitOrder);
 }
 
 export default orderSaga;
