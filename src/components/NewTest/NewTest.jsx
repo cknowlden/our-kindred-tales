04c500ad429918ce1c4268f30ebaf0a62b2d9301
Richard Barnitz <richardbarnitz@gmail.com>
2024-04-30 00:35:49 -0700

    made PDFMake file & folder, and JSON input field

--- src/components/NewTest/NewTest.jsx
+++ src/components/NewTest/NewTest.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import JSON from '../JSON/JSON';
 
 function NewTest() {
   //TO DO: function to dispatch to saga for submitting render for test JSON data
@@ -6,6 +7,7 @@ function NewTest() {
     <div>
       <h1>top section with editable table for project name and contact info</h1>
       <div>Enter JSON here to submit to PDF formatter</div>
+      <JSON />
       <center>
         <button className="btn">Submit</button>
       </center>
