cdf30459b271947901b63a0eff1d6f317b75e677
Cali Knowlden <calbearnc@gmail.com>
2024-04-30 19:42:01 -0500

    good get request for project for overview page

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(passport.session());
 
 // Routes
 app.use('/api/user', userRouter);
-app.use('/api/overview', overviewRouter);
+app.use('/api/overview', overviewRouter); //pulls project info into overview page
 
 // Listen Server & Port
 app.listen(PORT, () => {
