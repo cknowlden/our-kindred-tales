d284ebf15f131e1ce2184f4136616ec494558d9e
Daniel Holt <holt.daniel.b@gmail.com>
2024-04-30 21:41:58 -0400

    formating js of submit orders form

--- src/components/SubmitOrderForm/SubmitOrderForm.jsx
+++ src/components/SubmitOrderForm/SubmitOrderForm.jsx
@@ -1,21 +1,15 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
+import React from "react";
+import { useDispatch } from "react-redux";
 
 function SubmitOrderForm(props) {
   const dispatch = useDispatch();
   return (
     <div>
-    <button
-      onClick={() => dispatch({ type: 'SUBMIT_ORDER' })}
-    >
-      Submit
-    </button>
+      <button onClick={() => dispatch({ type: "SUBMIT_ORDER" })}>Submit</button>
 
-<button
-onClick={() => dispatch({ type: 'FETCH_ORDERS' })}
->
-Retrieve jobs
-</button>
+      <button onClick={() => dispatch({ type: "FETCH_ORDERS" })}>
+        Retrieve jobs
+      </button>
     </div>
   );
 }
