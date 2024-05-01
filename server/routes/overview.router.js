827623365a494f87d0eb4bdecb0734ca9f24e9e8
Cali Knowlden <calbearnc@gmail.com>
2024-04-30 20:18:17 -0500

    updated get route to limit items returned specific to overview page

--- server/routes/overview.router.js
+++ server/routes/overview.router.js
@@ -4,10 +4,13 @@ const router = express.Router();
 
 //pulls the project info from db
 router.get('/', (req, res) => {
+  // const queryText = `
+  //   SELECT * FROM "project_details"
+  //     ORDER BY "book_title";
+  //     `;
+
   const queryText = `
-    SELECT * FROM "project_details"
-      ORDER BY "book_title";
-      `;
+    SELECT "project_list".project_name, "project_list".contact, "project_list".last_updated, "project_list".status FROM "project_list" JOIN "project_details" ON "project_list".project_name = "project_details".book_title ORDER BY "project_details".book_title ASC;`;
   pool
     .query(queryText)
     .then((dbRes) => {
