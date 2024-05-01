2022d25d585faf1abd54f6728a7f8e96a069a8fd
Daniel Holt <holt.daniel.b@gmail.com>
2024-04-30 21:28:50 -0400

    data in proper format to be sent to lulu api for book creation. Required fields marked with required comment above them

--- src/redux/sagas/order.saga.js
+++ src/redux/sagas/order.saga.js
@@ -5,18 +5,21 @@ import axios from "axios";
 function* submitOrder(action) {
   const url = "https://api.sandbox.lulu.com";
   const grantData = "grant_type=client_credentials";
-  
+
   try {
-    let accessToken = '';
+    let accessToken = "";
     // Retrieve security token
     yield axios
       .post(
         `${url}/auth/realms/glasstree/protocol/openid-connect/token`,
-        grantData, {headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-          'Authorization': "Basic MjlmYTQzNDYtNTBiMC00NDRlLTgwNjUtYmNhOGMyOGMwMTMxOmRhOFB3NzBseVdJT2ZlUVg3TVpwMlVMNkw3cUNaOTlN",
-        }}
-        
+        grantData,
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            Authorization:
+              "Basic MjlmYTQzNDYtNTBiMC00NDRlLTgwNjUtYmNhOGMyOGMwMTMxOmRhOFB3NzBseVdJT2ZlUVg3TVpwMlVMNkw3cUNaOTlN",
+          },
+        }
       )
       .then((response) => {
         accessToken = response.data.access_token;
@@ -26,19 +29,57 @@ function* submitOrder(action) {
         console.error("Error:", error);
       });
     // Send order to lulu
-    const data = {};
-    yield 
-    axios.post(`${url}/print-jobs/`, data , {headers: {
-      'Authorization': `Bearer ${accessToken}`,
-      'Content-Type': 'application/json'
-    }})
-      .then(response => {
-        console.log('Response:', response.data);
+    const data = {
+      //required
+      contact_email: "test@test.com",
+      external_id: "demo-time",
+      //required
+      line_items: [
+        {
+          external_id: "item-reference-1",
+          printable_normalization: {
+            cover: {
+              source_url:
+                "https://www.dropbox.com/s/7bv6mg2tj0h3l0r/lulu_trade_perfect_template.pdf?dl=1&raw=1",
+            },
+            interior: {
+              source_url:
+                "https://www.dropbox.com/s/r20orb8umqjzav9/lulu_trade_interior_template-32.pdf?dl=1&raw=1",
+            },
+            pod_package_id: "0600X0900BWSTDPB060UW444MXX",
+          },
+          quantity: 30,
+          title: "My Book",
+        },
+      ],
+      production_delay: 120,
+      //required
+      shipping_address: {
+        city: "Lübeck",
+        country_code: "GB",
+        name: "Hans Dampf",
+        phone_number: "844-212-0689",
+        postcode: "PO1 3AX",
+        state_code: "",
+        street1: "Holstenstr. 48",
+      },
+      //required
+      shipping_level: "MAIL",
+    };
+    yield;
+    axios
+      .post(`${url}/print-jobs/`, data, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+        },
       })
-      .catch(error => {
-        console.error('Error:', error);
+      .then((response) => {
+        console.log("Response:", response.data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
       });
-  
   } catch (error) {
     console.log("Error with order submit:", error);
     yield put({ type: "ORDER_FAILED" });
