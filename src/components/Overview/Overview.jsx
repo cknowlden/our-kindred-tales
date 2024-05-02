0cc335d82cf6f915541f534f88a3c377765fd8ff
Cali Knowlden <calbearnc@gmail.com>
2024-05-01 20:29:33 -0500

    overview table now displaying information from database

--- src/components/Overview/Overview.jsx
+++ src/components/Overview/Overview.jsx
@@ -20,15 +20,15 @@ function Overview() {
     dispatch({ type: 'FETCH_PROJECTS' });
   }, []);
 
-  function createData(name, updated, status) {
-    return { name, updated, status };
-  }
+  // function createData(name, updated, status) {
+  //   return { name, updated, status };
+  // }
 
   //TO DO: Replace the below rows with actual project data from database
-  const rows = [
-    createData('Project #1', '2024-01-09', 'Customer Working'),
-    createData('Project #2', '2024-04-11', 'Ready for Print'),
-  ];
+  // const rows = [
+  //   createData('Project #1', '2024-01-09', 'Customer Working'),
+  //   createData('Project #2', '2024-04-11', 'Ready for Print'),
+  // ];
 
   return (
     <>
@@ -37,24 +37,26 @@ function Overview() {
           <TableHead>
             <TableRow>
               <TableCell>Project</TableCell>
+              <TableCell align="right">Contact</TableCell>
               <TableCell align="right">Last Updated</TableCell>
               <TableCell align="right">Status</TableCell>
               <TableCell align="right">Actions</TableCell>
               <TableCell align="right"></TableCell>
-              <TableCell align="right"></TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row) => (
+            {/* {rows.map((row) => ( */}
+            {projects.map((project) => (
               <TableRow
-                key={row.name}
+                key={project.contact}
                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
               >
                 <TableCell component="th" scope="row">
-                  {row.name}
+                  {project.project_name}
                 </TableCell>
-                <TableCell align="right">{row.updated}</TableCell>
-                <TableCell align="right">{row.status}</TableCell>
+                <TableCell align="right">{project.contact}</TableCell>
+                <TableCell align="right">{project.last_updated}</TableCell>
+                <TableCell align="right">{project.status}</TableCell>
                 <TableCell align="right">
                   <MenuIcon />
                 </TableCell>
@@ -71,12 +73,13 @@ function Overview() {
           </TableBody>
         </Table>
       </TableContainer>
-      <center>
+      {/* TO DO: add functionality for create new test project */}
+      {/* <center>
         <Link to="/new-test">
           <button className="btn">Create New Test Project</button>
         </Link>
-      </center>
-      <h1>{JSON.stringify(projects)}</h1>
+      </center> */}
+      {/* <h1>{JSON.stringify(projects)}</h1> */}
     </>
   );
 }
