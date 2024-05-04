d4758f2f495cb9699beca680a717c086fce347fe
Cali Knowlden <calbearnc@gmail.com>
2024-05-03 21:21:43 -0500

    good delete route for deleting a project from overview page

--- server/routes/overview.router.js
+++ server/routes/overview.router.js
@@ -4,10 +4,6 @@ const router = express.Router();
 
 //pulls the project info from db
 router.get('/', (req, res) => {
-  // const queryText = `
-  //   SELECT "project_list".project_name, "project_list".contact, "project_list".last_updated, "project_list".status FROM "project_list" JOIN "project_details" ON "project_list".project_name = "project_details".book_title ORDER BY "project_details".book_title ASC;
-  //   `;
-
   const queryText = `
   SELECT "project_list".project_id, "project_list".project_name, "project_list".contact, "project_list".last_updated, "project_list".status FROM "project_list" JOIN "project_details" ON "project_list".project_id = "project_details".id ORDER BY "project_details".book_title ASC;
   `;
@@ -22,4 +18,19 @@ router.get('/', (req, res) => {
     });
 });
 
+router.delete('/:id', (req, res) => {
+  pool
+    .query(
+      `
+    DELETE FROM "project_list" WHERE project_id=$1;`,
+      [req.params.id]
+    )
+    .then((result) => {
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.log('Error DELETING project', error);
+      res.sendStatus(500);
+    });
+});
 module.exports = router;
