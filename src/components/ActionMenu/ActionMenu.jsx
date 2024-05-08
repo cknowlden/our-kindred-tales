21304a853baa4cf424a057fb463f0e47af3095b6
Jan Michael Biyo <janmichael.biyo@gmail.com>
2024-05-07 22:04:33 -0400

    had to test

--- src/components/ActionMenu/ActionMenu.jsx
+++ src/components/ActionMenu/ActionMenu.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-import Button from "@mui/material/Button";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import MenuIcon from "@mui/icons-material/Menu";
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function ActionMenu() {
   const dispatch = useDispatch();
@@ -31,9 +31,9 @@ function ActionMenu() {
     <div>
       <Button
         id="basic-button"
-        aria-controls={open ? "basic-menu" : undefined}
+        aria-controls={open ? 'basic-menu' : undefined}
         aria-haspopup="true"
-        aria-expanded={open ? "true" : undefined}
+        aria-expanded={open ? 'true' : undefined}
         onClick={handleClick}
       >
         <MenuIcon />
@@ -44,13 +44,13 @@ function ActionMenu() {
         open={open}
         onClose={handleClose}
         MenuListProps={{
-          "aria-labelledby": "basic-button",
+          'aria-labelledby': 'basic-button',
         }}
       >
         <MenuItem onClick={handleClose}>Send to client for review</MenuItem>
         <MenuItem onClick={handleClose}>Create printable PDF</MenuItem>
         <MenuItem onClick={handleClose}>
-          <p onClick={() => dispatch({ type: "SUBMIT_ORDER" })}>
+          <p onClick={() => dispatch({ type: 'SUBMIT_ORDER' })}>
             Send to Publisher
           </p>
         </MenuItem>
