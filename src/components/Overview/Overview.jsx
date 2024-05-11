d1b582bfb8c6aad58a4132fbeff86c00a3800fde
Cali Knowlden <calbearnc@gmail.com>
2024-05-11 17:44:07 -0500

    altered timestamp

--- src/components/Overview/Overview.jsx
+++ src/components/Overview/Overview.jsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-// import { Link } from 'react-router-dom';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -12,64 +11,39 @@ import Paper from '@mui/material/Paper';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import ActionMenu from '../ActionMenu/ActionMenu';
 import Swal from 'sweetalert2';
-import { IconButton } from '@mui/material';
-
-import '../App/App.css';
 
 function Overview() {
   const projects = useSelector((store) => store.projects);
   const dispatch = useDispatch();
   const history = useHistory();
 
-  // const eventDetails = useSelector((store) => store.details);
-  // const details = eventDetails[0] || 'No details available';
-  const projectTime = projects[0] || 'unavailable';
-  const date = new Date(projectTime.date);
-  // const date = new Date(details.date);
-  const formattedDate = date.toLocaleDateString('en-US', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    hour12: true,
-  });
-
-  const displayProject = (projectDisplay) => {
-    console.log(projectDisplay);
-    dispatch({ type: 'SET_PROJECT_DETAILS', payload: projectDisplay });
-    history.push(`/details/${projectDisplay.id}`);
+  const displayProject = (project) => {
+    dispatch({ type: 'SET_PROJECT_DETAILS', payload: project });
+    history.push(`/details/${project.id}`);
   };
 
-  const handleDelete = (event) => {
-    console.log(event.target.id);
-    dispatch({
-      type: 'DELETE_PROJECT',
-      payload: {
-        targetId: event.target.id,
-      },
-    });
-  };
-
-  const showConfirmationDelete = (event) => {
-    event.preventDefault();
+  const handleDelete = (projectId) => {
     Swal.fire({
-      text: 'Are you sure you want to delete this event?',
+      text: 'Are you sure you want to delete this project?',
       icon: 'warning',
       showCancelButton: true,
       confirmButtonText: 'Yes, delete it!',
       cancelButtonText: 'No, keep it',
     }).then((result) => {
       if (result.isConfirmed) {
-        Swal.fire('The event has been deleted').then(() => handleDelete(event));
-      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        dispatch({ type: 'DELETE_PROJECT', payload: projectId });
       }
     });
   };
 
+  const showConfirmationDelete = (projectId) => {
+    event.preventDefault();
+    handleDelete(projectId);
+  };
+
   useEffect(() => {
     dispatch({ type: 'FETCH_PROJECTS' });
-  }, []);
+  }, [dispatch]);
 
   return (
     <>
@@ -77,39 +51,38 @@ function Overview() {
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell>Project</TableCell>
+              <TableCell>Project Name</TableCell>
               <TableCell align="right">Contact</TableCell>
               <TableCell align="right">Last Updated</TableCell>
               <TableCell align="right">Status</TableCell>
-              <TableCell align="right"></TableCell>
-              <TableCell align="right"></TableCell>
+              <TableCell align="right">Page Count</TableCell>
+              <TableCell align="right">Actions</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {/* {rows.map((row) => ( */}
             {projects.map((project) => (
               <TableRow
-                key={projects.contact}
+                key={project.id}
                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
               >
                 <TableCell
                   className="tr"
-                  onClick={(event) => displayProject(project)}
+                  onClick={() => displayProject(project)}
                   component="th"
                   scope="row"
                 >
-                  {project.project_name}
+                  {project.project_name || project.bookTitle}
                 </TableCell>
                 <TableCell align="right">{project.contact}</TableCell>
-                <TableCell align="right">{formattedDate}</TableCell>
+                <TableCell align="right">{project.last_updated}</TableCell>
                 <TableCell align="right">{project.status}</TableCell>
+                <TableCell align="right">{project.page_count}</TableCell>
                 <TableCell align="right">
-                  <ActionMenu />
+                  <ActionMenu id={project.id} />
                 </TableCell>
                 <TableCell align="right">
                   <DeleteOutlineIcon
-                    onClick={showConfirmationDelete}
-                    id={project.project_id}
+                    onClick={() => showConfirmationDelete(project.id)}
                     aria-label="delete"
                     color="primary"
                     size="large"
@@ -120,13 +93,6 @@ function Overview() {
           </TableBody>
         </Table>
       </TableContainer>
-      {/* TO DO: add functionality for create new test project */}
-      {/* <center>
-        <Link to="/new-test">
-          <button className="btn">Create New Test Project</button>
-        </Link>
-      </center> */}
-      {/* <h1>{JSON.stringify(projects)}</h1> */}
     </>
   );
 }
