9adb8717e294171dfbc3f3b75006190ac53d2b0f
Cali Knowlden <calbearnc@gmail.com>
2024-05-11 17:47:06 -0500

    converted timestamp to human readable format

--- src/components/Overview/Overview.jsx
+++ src/components/Overview/Overview.jsx
@@ -17,6 +17,17 @@ function Overview() {
   const dispatch = useDispatch();
   const history = useHistory();
 
+  const projectTime = projects[0] || 'Date unavailable';
+  const date = new Date(projectTime.last_updated);
+  const formattedDate = date.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true,
+  });
+
   const displayProject = (project) => {
     dispatch({ type: 'SET_PROJECT_DETAILS', payload: project });
     history.push(`/details/${project.id}`);
@@ -74,7 +85,7 @@ function Overview() {
                   {project.project_name || project.bookTitle}
                 </TableCell>
                 <TableCell align="right">{project.contact}</TableCell>
-                <TableCell align="right">{project.last_updated}</TableCell>
+                <TableCell align="right">{formattedDate}</TableCell>
                 <TableCell align="right">{project.status}</TableCell>
                 <TableCell align="right">{project.page_count}</TableCell>
                 <TableCell align="right">
