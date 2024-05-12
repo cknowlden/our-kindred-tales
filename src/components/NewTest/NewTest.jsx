5fc0f1604d71f8aaa8bb7aa97be17f468ac38f22
Cali Knowlden <calbearnc@gmail.com>
2024-05-11 20:27:19 -0500

    deleted old code from newtest

--- src/components/NewTest/NewTest.jsx
+++ src/components/NewTest/NewTest.jsx
@@ -1,31 +1,3 @@
-// import React, { useState } from 'react';
-// import JSON from '../JSON/JSON';
-// import PDFMake from '../PDFMake/PDFMake';
-// import testJson from '../PDFMake/testfile.json';
-// import { useHistory } from 'react-router-dom';
-
-// function NewTest() {
-//   const [jsonData, setJsonData] = useState(null);
-//   const history = useHistory();
-
-//   const handleJSONSubmit = (data) => {
-//     // Handle JSON submission and update jsonData state
-//     //console.log('Submitted JSON Data:', data);
-//     setJsonData(data);
-//     // history.push('/overview');
-//   };
-
-//   return (
-//     <div>
-//       <h1>Top section with editable table for project name and contact info</h1>
-//       <JSON onSubmit={handleJSONSubmit} /> {/* Pass callback function */}
-//       {jsonData && <PDFMake jsonData={testJson} />}{' '}
-//     </div>
-//   );
-// }
-
-// export default NewTest;
-
 import React, { useState, useEffect } from 'react';
 import JSON from '../JSON/JSON';
 import PDFMake from '../PDFMake/PDFMake';
