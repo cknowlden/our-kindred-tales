fa92ea0b28a3ef209b7f86a32c05730a61220577
LuAnn Yang <luann.yang@gmail.com>
2024-05-13 20:13:36 -0500

    Delete Saga. Working Delete for GCS and Local DB

--- src/redux/sagas/delete.saga.js
+++ src/redux/sagas/delete.saga.js
@@ -2,17 +2,28 @@ import { put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
 
 function* deleteProject(action) {
-  console.log(action.payload);
   try {
-    yield axios.delete(`/api/overview/${action.payload.targetId}`);
+    const targetId = action.payload.targetId;
+    console.log('Target ID for deletion:', targetId);
 
-    yield put({ type: 'FETCH_PROJECTS', payload: action.payload.data });
+    const url = `/api/overview/${targetId}`;
+    console.log('Delete request URL:', url);
+
+    yield axios.delete(url);
+    yield put({ type: 'DELETE_PROJECT_SUCCESS', payload: action.payload });
+
+
+    yield put({ type: 'SET_DELETE_PROJECT', payload: targetId });
+    yield put({ type: 'FETCH_PROJECTS' });
   } catch (error) {
     console.log('Project could not be deleted', error);
+    yield put({ type: 'DELETE_PROJECT_FAILURE', payload: error });
   }
 }
+
 function* deleteSaga() {
   yield takeLatest('DELETE_PROJECT', deleteProject);
 }
 
 export default deleteSaga;
+
